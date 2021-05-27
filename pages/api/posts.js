baae8b60c211c991ae63cdfe6ef978091be61a4e
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-05-27 17:43:49 -0300

    02-Creating an api route

---
+++ pages/api/posts.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const handler = async(req,res) => {
+    if(req.method === 'GET'){
+        try{
+            const request =await axios.get('https://jsonplaceholder.typicode.com/posts');
+
+            res.status(200).json({
+                posts:request.data
+            });
+        } catch(error){
+            res.status(401).json({ message:'Opps i did it again'})
+        }
+    }
+}
+
+
+export default handler;
