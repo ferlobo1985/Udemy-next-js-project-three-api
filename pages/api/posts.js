27e3af9818cab7cbb3a64f4c944b447ec6323f3a
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-05-27 18:21:56 -0300

    03-creating a post request

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,17 +1,25 @@
 import axios from 'axios';
-inmsdsd
+
 
 const handler = async(req,res) => {
-    if(req.method === 'GET'){
+    if(req.method === 'POST'){
+        const { title, body } = req.body;
+
         try{
-            const request =await axios.get('https://jsonplaceholder.typicode.com/posts');
+            const request = await axios.post('https://jsonplaceholder.typicode.com/posts',{
+                title,
+                body
+            });
 
             res.status(200).json({
-                posts:request.data
-            });
+                posts: request.data
+            })
         } catch(error){
-            res.status(401).json({ message:'Opps i did it again'})
+            res.status(401).json({
+                message:'Sorry, try again later'
+            })
         }
+
     }
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,46 @@
 import { useState } from 'react';
+import { useFormik } from 'formik';
 import axios from 'axios';
 
 const Home = (props) => {
-  const [post,setPost] =useState([]);
-  const getPosts = () => {
-    axios.get('/api/posts').then(response =>{
-      setPost(response.data)
-    })
-  }
+  const [post,setPost] = useState([]);
 
-  console.log(post)
+  const formik = useFormik({
+    initialValues:{
+      title:'',
+      body:''
+    },
+    onSubmit: values => {
+      console.log(values)
+    }
+  })
 
   return (
     <div>
-      <button onClick={getPosts}>
-        Load posts
-      </button>
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label><br/>
+          <input
+            id="title"
+            name="title"
+            onChange={formik.handleChange}
+            value={formik.values.title}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="title">Body</label><br/>
+          <input
+            id="body"
+            name="body"
+            onChange={formik.handleChange}
+            value={formik.values.body}
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+
+      </form>
     </div>
   )
 }
