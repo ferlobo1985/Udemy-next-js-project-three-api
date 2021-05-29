fec6e98478f5ca5727b7ae73944e2f6f6c5d48b2
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-05-29 17:49:20 -0300

    config

--- helpers/database/mongo-db.js
+++ helpers/database/mongo-db.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb'
 
 export async function connectToDb(){
-    const dbClient = await MongoClient.connect('mongodb+srv://admin:aoucGFHHfDP28t7X@cluster0.rc4ay.mongodb.net/testingapi?retryWrites=true&w=majority');
+    const dbClient = await MongoClient.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rc4ay.mongodb.net/testingapi?retryWrites=true&w=majority`);
     return dbClient;
 }
 
